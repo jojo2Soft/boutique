d7bcf0a9f2ae0b6e3ba52d2731bf61372cee23de
josue agboton <josueagbotonking@gmail.com>
2024-09-18 01:25:42 +0100

    first version of project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="h1">hello word</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../assets/base.scss';
+@import '../assets/debug.scss';
+</style>
